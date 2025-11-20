7794fa4bc0d2fbaab217324635e3875b558f4da9
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2025-11-20 18:21:18 -0400

    fix: macro to generate implementation for setting structs

--- src/settings.rs
+++ src/settings.rs
@@ -3,12 +3,15 @@ use std::ops::{Deref, DerefMut};
 use serde::{Deserialize, Serialize};
 use ui::FileItem;
 
+use crate::user_data::Save;
+
 macro_rules! impl_deref {
-    ($($n:ty ( $t:ty ) : $f:literal),*) => {
+    ($( $n:ident ( $t:ty ) : $f:literal ),*) => {
+        $(
         #[derive(Default, Deserialize, Serialize)]
         pub struct $n($t);
 
-        $(impl Deref for $n {
+        impl Deref for $n {
             type Target = $t;
 
             fn deref(&self) -> &Self::Target {
@@ -20,15 +23,16 @@ macro_rules! impl_deref {
             fn deref_mut(&mut self) -> &mut Self::Target {
                 &mut self.0
             }
-        })*
+        }
 
         impl Save for $n {
             const NAME: &str = $f;
         }
+        )*
     };
 }
 
 impl_deref! {
     FavoriteTexts(Vec<String>): "fav_texts",
-    SourceSongs(Vec<FileItem>): "songs",
+    SourceSongs(Vec<FileItem>): "source_songs"
 }
